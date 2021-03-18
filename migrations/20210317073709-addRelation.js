71afa75759f8f18857969348e218b26001f6102a
Ubuntu <ubuntu@ip-172-31-35-131.ap-northeast-2.compute.internal>
2021-03-18 02:23:49 +0000

    🐞  버그 : 테이블 이름을 대문자로 하지 않아서 발생한 오류 수정

--- migrations/20210317073709-addRelation.js
+++ migrations/20210317073709-addRelation.js
@@ -2,58 +2,58 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addConstraint("user_recipe_joins", {
+    await queryInterface.addConstraint("User_Recipe_joins", {
       fields: ["userId"],
       type: "foreign key",
       name: "user_recipe",
       references: {
-        table: "users",
+        table: "Users",
         field: "id",
       },
     });
-    await queryInterface.addConstraint("user_recipe_joins", {
+    await queryInterface.addConstraint("User_Recipe_joins", {
       fields: ["recipeId"],
       type: "foreign key",
       name: "recipe_user",
       references: {
-        table: "recipes",
+        table: "Recipes",
         field: "id",
       },
     });
-    await queryInterface.addConstraint("user_course_joins", {
+    await queryInterface.addConstraint("User_Course_joins", {
       fields: ["userId"],
       type: "foreign key",
       name: "user_course",
       references: {
-        table: "users",
+        table: "Users",
         field: "id",
       },
     });
-    await queryInterface.addConstraint("user_course_joins", {
+    await queryInterface.addConstraint("User_Course_joins", {
       fields: ["userId"],
       type: "foreign key",
       name: "course_user",
       references: {
-        table: "courses",
+        table: "Courses",
         field: "id",
       },
     });
-    await queryInterface.addConstraint("recipes", {
+    await queryInterface.addConstraint("Recipes", {
       fields: ["courseId"],
       type: "foreign key",
       name: "course_recipe",
       references: {
-        table: "courses",
+        table: "Courses",
         field: "id",
       },
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeConstraint("user_recipe_joins", "user_recipe");
-    await queryInterface.removeConstraint("user_recipe_joins", "recipe_user");
-    await queryInterface.removeConstraint("user_course_joins", "user_course");
-    await queryInterface.removeConstraint("user_course_joins", "course_user");
-    await queryInterface.removeConstraint("recipes", "course_recipe");
+    await queryInterface.removeConstraint("User_Recipe_joins", "user_recipe");
+    await queryInterface.removeConstraint("User_Cecipe_joins", "recipe_user");
+    await queryInterface.removeConstraint("User_Course_joins", "user_course");
+    await queryInterface.removeConstraint("User_Course_joins", "course_user");
+    await queryInterface.removeConstraint("Recipes", "course_recipe");
   },
 };
