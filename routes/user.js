38818914f19717de0899d709e208dd0b5898c221
osunguk(mac) <osunguk@gmail.com>
2021-03-21 20:25:55 +0900

    🔨 수정 : login route 변경

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/user');
+const controller = require("../controllers/user");
 
-router.post('/login', controller.login);
+router.post("/login", controller.login);
 
-router.post('/logout', controller.logout);
+router.post("/logout", controller.logout);
 
-router.post('/register', controller.register);
+router.post("/register", controller.register);
 
-router.get('/:id/info', controller.info);
+router.get("/info", controller.info);
 
-router.post('/:id/update', controller.update);
+router.post("/:id/update", controller.update);
 
-router.post('/:id/unregister', controller.unregister);
+router.post("/:id/unregister", controller.unregister);
 
 module.exports = router;
