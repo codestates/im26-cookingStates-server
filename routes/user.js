4e1b98572c5e3ac05a12d8d1c4a32d735a62d855
osunguk(mac) <osunguk@gmail.com>
2021-03-23 14:16:36 +0900

    🔨 수정 : API 에서 user ID 를 받아 라우팅하는 부분 제거

--- routes/user.js
+++ routes/user.js
@@ -10,9 +10,9 @@ router.post("/register", controller.register);
 
 router.get("/info", controller.info);
 
-router.post("/:id/update", controller.update);
+router.post("/update", controller.update);
 
-router.post("/:id/unregister", controller.unregister);
+router.post("/unregister", controller.unregister);
 
 router.post("/checkemail", controller.checkemail);
 
